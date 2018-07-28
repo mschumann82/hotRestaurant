6f12ceebc418af20ef1f553ac8886a3c031b5d61
Michael Schumann <schumann82@gmail.com>
2018-07-28 10:17:45 -0700

    requires

--- restaurant.js
+++ restaurant.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
